28ee1da043c530101c8f4a9a223ece3dbec98385
emno <emno@itu.dk>
2023-09-28 10:32:50 +0200

    solve 5.1

---
+++ mergeC/Program.cs
@@ -0,0 +1,14 @@
+﻿static int[] Merge (int[] xs, int[] ys){
+    int[] output = new int[xs.Length + ys.Length];
+
+    for (int i = 0; i< xs.Length; i++) output[i] = xs[i];
+    for (int i = 0; i< ys.Length; i++) output[xs.Length + i] = ys[i];
+
+    Array.Sort(output);
+    return output;
+}
+
+
+int[] a = { 1, 2 , 66};
+int[] b = { -100, 2 , 50};
+foreach(var x in Merge(a,b)){ Console.WriteLine(x);}
---
+++ mergeF/Program.fs
@@ -0,0 +1,10 @@
+﻿
+(* The runtime of this function belongs to O(n log n).
+   There exists a variant that runs in constant time, 
+   but this has not been implemented
+*)
+let merge tuple = 
+    let first = fst tuple
+    let second = snd tuple
+
+    first @ second |> List.sort
